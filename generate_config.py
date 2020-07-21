92ae5af4deb44d268ba44c035f72a6fb29eadaa6
DGreem75 <67576501+DGreem75@users.noreply.github.com>
2020-07-21 21:47:46 +0200

    Add files via upload

---
+++ generate_config.py
@@ -0,0 +1,28 @@
+# -*-coding:Latin-1 -*
+import os
+os.chdir("D:/script cours/OCprojet6")
+
+# MENU
+# Créer un site
+# Créer un routeur
+# Créer un switch
+
+## CREATION SITE
+# Saisir n° site
+# Afficher VLAN2
+# Afficher VLAN3
+# Afficher VLAN99
+# Creation fichier sxxx.txt  xxx = n° du site
+
+# Création routeur
+# Saisir le n° du site
+# => vérifier si site existe et différent du site 1 (site mère)
+# => vérifier si routeur à créer n'existe pas (test ping par exemple)
+# => genérer configuration routeur du site n° saisi
+
+# Creation SWITCH
+# Saisir le n° du site
+# => vérifier si site existe
+# Saisir si switch niveau 1 ou 2
+# => vérifier si switch à créer n'existe pas (test ping par exemple)
+# => genérer configuration switch du site n° saisi
