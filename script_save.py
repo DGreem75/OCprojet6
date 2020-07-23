426144c870a221daa22083c0e6686f0d91c64ddb
DGreem75 <dgreem75+git@gmail.com>
2020-07-23 10:59:59 +0200

    ajout script sav

---
+++ script_save.py
@@ -0,0 +1,8 @@
+# -*-coding:Latin-1 -*
+import os
+os.chdir("D:/script cours/OCprojet6")
+
+# SCRIPT DE BACKUP DES SWITCHS ET ROUTEURS INSTALLES"
+# Lister les devices pour backup
+# Pour chaque devices faire un backup FTP / SSH ??
+# Si pas OK => renseigner log pas ok
